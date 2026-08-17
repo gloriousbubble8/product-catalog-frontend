7f7f9cc2cb4c09f153c072d87c2dfb13dfbf4817
Giridhar <balinenigiri8@gmail.com>
2026-08-17 23:26:57 +0530

    Applied styles to the product catalog

--- scripts/script1.js
+++ scripts/script1.js
@@ -6,7 +6,7 @@ async function getProducts(){
 
 function displayProducts(products){
     let htmlString = products
-            .map(product => createProductCard(product))
+            .map(product => createProductCard1(product))
             .join("");
     
     document.getElementById("pdc").innerHTML=htmlString;       
@@ -22,4 +22,19 @@ function createProductCard(product){
             `
 }
 
-getProducts();
+function createProductCard1(product){
+    return  `
+            <div id="pc_container">
+            <div id="timg">
+                <img src=${product.images[0]}>
+            </div>
+            <div id="pdesc">
+                <p>${product.title}</p>
+                <p><strong>$ ${product.price}</strong></p>
+                <a href="details.html" target="_blank" id="dtlsa">
+                    <span id="dtls">See More details</span>
+                </a>
+            </div>
+        </div>
+            `;
+}            
